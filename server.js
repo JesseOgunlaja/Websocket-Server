3ca481ff529e069a44dab3f0044fed9782e887ef
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-11-23 11:08:52 +0000

    Make production ready

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8000",
+    origin: process.env.CORS_ORIGIN || "http://localhost:8000",
   },
 });
+
+const PORT = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
   console.log("User connected");
@@ -20,6 +22,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("Listening on port", 5000);
+server.listen(PORT, () => {
+  console.log("Listening on port", PORT);
 });
