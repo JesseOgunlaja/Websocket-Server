5fac886679b9927707ce8cf5f544247977c70d2f
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2023-11-23 11:15:29 +0000

    Update CORS policy

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const http = require("http");
+const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:8000",
-  },
-});
+const io = new Server(server);
 
 const PORT = process.env.PORT || 5000;
 
