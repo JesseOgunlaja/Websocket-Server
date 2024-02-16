2ede79a3a002f6a96355ead2ddeb62658a18758b
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2024-02-16 15:23:57 +0000

    Attempt to fix bug

--- server.js
+++ server.js
@@ -52,14 +52,17 @@ fastify.ready().then(() => {
   });
 });
 
-try {
-  const PORT = process.env.PORT || 5000;
-  fastify.listen({
-    port: PORT,
-    host: "0.0.0.0",
-  });
-  console.log(`Listening on port ${PORT}`);
-} catch (err) {
-  fastify.log.error(err);
-  process.exit(1);
-}
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 5000;
+    await fastify.listen({
+      port: PORT,
+    });
+    console.log(`Listening on port ${PORT}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
