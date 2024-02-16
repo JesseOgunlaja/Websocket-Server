7303c55ba291f48e873647edaf58b8104a9ff7b7
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2024-02-16 11:45:28 +0000

    Change to Fastify

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 require("dotenv").config();
-const express = require("express");
-const http = require("http");
-const app = express();
+const fastify = require("fastify")();
+const fastifySocketIo = require("fastify-socket.io");
+const fastifyCors = require("@fastify/cors");
 const CryptoJS = require("crypto-js");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server, {
+
+fastify.register(fastifyCors, {
+  origin: "*",
+});
+
+fastify.register(fastifySocketIo, {
   cors: {
     origin: "*",
   },
 });
-
-app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
 
 function decryptString(value) {
   const decrypted = CryptoJS.AES.decrypt(
@@ -23,34 +22,43 @@ function decryptString(value) {
   return decrypted;
 }
 
-app.post("/emit-event", (req, res) => {
-  const { id, event, msg, password } = req.body;
+fastify.post("/emit-event", (request, reply) => {
+  const { id, event, msg, password } = request.body;
 
   if (!id || !event || !msg || !password) {
-    return res.status(400).json({ message: "Missing values" });
+    return reply.status(400).send({ message: "Missing values" });
   }
 
   if (password !== process.env.WEBSOCKET_KEY) {
-    return res.status(401).json({ message: "Invalid password" });
+    return reply.status(401).send({ message: "Invalid password" });
   }
 
   console.log(id, event, msg);
 
-  io.to(id).emit(event, msg);
+  setTimeout(() => fastify.io.to(id).emit(event, msg), 1);
 
-  return res.status(200).json({ message: "Event emitted successfully" });
+  return reply.status(200).send({ message: "Event emitted successfully" });
 });
 
-io.use((socket, next) => {
-  const token = socket.handshake.auth.token;
-  const id = socket.handshake.query.id;
-  if (token && decryptString(token) === id) {
-    socket.join(id);
-    return next();
-  }
-  return new Response("Unauthorised");
+fastify.ready().then(() => {
+  fastify.io.use((socket, next) => {
+    const token = socket.handshake.auth.token;
+    const id = socket.handshake.query.id;
+    if (token && decryptString(token) === id) {
+      socket.join(id);
+      return next();
+    }
+    return new Error("Unauthorized");
+  });
 });
 
-server.listen(PORT, () => {
-  console.log("Listening on port", PORT);
-});
+try {
+  const PORT = process.env.PORT || 5000;
+  fastify.listen({
+    port: PORT,
+  });
+  console.log(`Listening on port ${PORT}`);
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
