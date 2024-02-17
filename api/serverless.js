0beb38dc302c1424b83410a4a030537fa8dbb559
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2024-02-17 13:37:00 +0000

    Try to make work on Vercel

--- api/serverless.js
+++ api/serverless.js
@@ -7,8 +7,22 @@ const app = Fastify({
   logger: true,
 });
 
-app.register(import("../server/server"), {
-  prefix: "/",
+app.get("/emit-event", (request, reply) => {
+  const { id, event, msg, password } = request.body;
+
+  if (!id || !event || !msg || !password) {
+    return reply.status(400).send({ message: "Missing values" });
+  }
+
+  if (password !== process.env.WEBSOCKET_KEY) {
+    return reply.status(401).send({ message: "Invalid password" });
+  }
+
+  console.log(id, event, msg);
+
+  // fastify.io.to(id).emit(event, msg);
+
+  return reply.status(200).send({ message: "Event emitted successfully" });
 });
 
 export default async (req, res) => {
